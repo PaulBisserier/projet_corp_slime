f08b1760ac622e630a4c5f7849fe272660230519
Pat <52626321+Pat-81@users.noreply.github.com>
2021-10-27 11:48:33 +0200

    Mise en place du projet v1

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CDN Fontawesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <!-- CDN Bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- css-->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Projet SLIME CORP</title>
+</head>
+
+<body>
+    
+    <?php require_once 'header.php' ?>
+    
+    <main>
+        <h1 id="titre" class="text-center py-3 text-success">SLIME CORP</h1>
+    </main>
+    
+    <?php require_once 'footer.php' ?>
+    <!--CDN JQUERY-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+    <!--script-->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
